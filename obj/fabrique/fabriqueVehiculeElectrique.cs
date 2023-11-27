d705297eaf20aa57bc7dfd9c5396f5877f4d5917
Rémi Lecas <lecasremi@gmail.com>
2023-11-27 14:02:19 +0100

    Ajout des fonction de fabriquation de véhicule

--- obj/fabrique/fabriqueVehiculeElectrique.cs
+++ obj/fabrique/fabriqueVehiculeElectrique.cs
@@ -1,3 +1,9 @@
 public class FabriqueVehiculeElectrique : Fabrique(){
+    public createAutomobile(){
+        return new AutomobileElectrique();
+    }
 
+    public createScooter(){
+        return new ScooterElectrique();
+    }
 }
--- obj/fabrique/fabriqueVehiculeEssence.cs
+++ obj/fabrique/fabriqueVehiculeEssence.cs
@@ -1,3 +1,9 @@
 public class FabriqueVehiculeEssence : Fabrique(){
-    
+    public createAutomobile(){
+        return new AutomobileEssence();
+    }
+
+    public createScooter(){
+        return new ScooterEssence();
+    }
 }
